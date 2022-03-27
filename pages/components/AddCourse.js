af155b6bb10d39373ccd250dd7834b0809ce3e81
divyanshu10072001 <tripathydivyanshu@iitgn.ac.in>
2022-03-27 09:59:29 +0530

    Added Search Bar

--- pages/components/AddCourse.js
+++ pages/components/AddCourse.js
@@ -1,95 +1,137 @@
-import * as React from 'react';
+import { useState } from "react";
+import * as React from "react";
 import styled from "styled-components";
-import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
-import Checkbox from '@mui/material/Checkbox';
-import { doc, setDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import Checkbox from "@mui/material/Checkbox";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
-export default function AddCourseModal({uid, handleClose, setdata}) {
+export default function AddCourseModal({ uid, handleClose, setdata }) {
   const courses = [
-    {course_code: 'HS421', course_name: 'ISE', counted_towards: '-', credits:4},
-    {course_code: 'CS614', course_name: 'Advanced Algorithms', counted_towards: '-', credits:4},
-    {course_code: 'CS328', course_name: 'Data Science', counted_towards: '-', credits:4},
-  ]
+    {
+      course_code: "HS421",
+      course_name: "ISE",
+      counted_towards: "-",
+      credits: 4,
+    },
+    {
+      course_code: "CS614",
+      course_name: "Advanced Algorithms",
+      counted_towards: "-",
+      credits: 4,
+    },
+    {
+      course_code: "CS328",
+      course_name: "Data Science",
+      counted_towards: "-",
+      credits: 4,
+    },
+  ];
 
-  const addCourse = async (course) =>{
+  const [disp, setdisp] = useState(courses);
+
+  const addCourse = async (course) => {
     let docRef = doc(db, "students", uid, "coursesOpted", course.course_code);
     await setDoc(docRef, {
       name: course.course_name,
       credits: course.credits,
-      countedTowards: course.counted_towards
+      countedTowards: course.counted_towards,
     });
     handleClose();
     setdata([]);
-  }
+  };
+
+  const handleSearch = (query) => {
+    query = query.toLowerCase();
+    if (query === "") {
+      setdisp(courses);
+    }
+    let arr = [];
+    for (let i = 0; i < courses.length; i++) {
+      let cur = courses[i];
+      if (
+        cur.course_code.toLowerCase().includes(query) ||
+        cur.course_name.toLowerCase().includes(query)
+      ) {
+        arr.push(cur);
+      }
+    }
+    setdisp(arr);
+  };
+
   return (
-    <div style={{width: '500px'}}>
-        <SearchBar>
-          <SearchIcon style={{marginTop: '3px', cursor: 'pointer'}}/>
-          <Input type="text" placeholder='Search...'/>
-        </SearchBar>
-        <Courses>
-          <Title>
-            <span>Course Code</span>
-            <span>Course Name</span>
-            <span>Course Towards</span>
-            <span>Credits</span>
-          </Title>
-          {
-            courses.map((course,ind) =>(
-              <Course onClick={() => addCourse(course)} style={ind%2===1 ? {backgroundColor: '#e5e5e5'} : {}}>
-                <span>{course.course_code}</span>
-                <span>{course.course_name}</span>
-                <span>{course.counted_towards}</span>
-                <span>{course.credits}</span>
-              </Course>
-            ))
-          }
-        </Courses>
+    <div style={{ width: "500px" }}>
+      <SearchBar>
+        <SearchIcon style={{ marginTop: "3px", cursor: "pointer" }} />
+        <Input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </SearchBar>
+      <Courses>
+        <Title>
+          <span>Course Code</span>
+          <span>Course Name</span>
+          <span>Course Towards</span>
+          <span>Credits</span>
+        </Title>
+        {disp.map((course, ind) => (
+          <Course
+            onClick={() => addCourse(course)}
+            style={ind % 2 === 1 ? { backgroundColor: "#e5e5e5" } : {}}
+          >
+            <span>{course.course_code}</span>
+            <span>{course.course_name}</span>
+            <span>{course.counted_towards}</span>
+            <span>{course.credits}</span>
+          </Course>
+        ))}
+      </Courses>
     </div>
   );
 }
 
 const SearchBar = styled.div`
-    width: 100%;
-    padding: 5px;
-    display: flex;
-    margin-left: -8px;
+  width: 100%;
+  padding: 5px;
+  display: flex;
+  margin-left: -8px;
 `;
 
 const Input = styled.input`
-    width: 100%;
-    padding: 5px;
-    font-size: 16px;
-    border: 1px solid;
-    border-radius: 3px;
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border: 1px solid;
+  border-radius: 3px;
 `;
 
 const Courses = styled.div`
-    width: 100%;
-    border: 1px solid;
-    border-radius: 3px;
-    margin-top: 10px;
+  width: 100%;
+  border: 1px solid;
+  border-radius: 3px;
+  margin-top: 10px;
 `;
 
 const Title = styled.div`
-    background-color: lightblue;
-    margin-top: 0px;
-    padding: 10px;
-    justify-content: space-between;
-    display: flex;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+  background-color: lightblue;
+  margin-top: 0px;
+  padding: 10px;
+  justify-content: space-between;
+  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 `;
 
 const Course = styled.div`
-    justify-content: space-between;
-    display: flex;
-    padding: 10px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    &:hover{
-      cursor: pointer;
-    }
+  justify-content: space-between;
+  display: flex;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  &:hover {
+    cursor: pointer;
+  }
 `;
--- pages/components/Table.js
+++ pages/components/Table.js
@@ -1,25 +1,24 @@
-import { useTable } from 'react-table';
-import React, {useState, useEffect} from 'react';
-import Button from '@mui/material/Button';
-import { Box } from '@mui/system';
+import { useTable } from "react-table";
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/system";
 import styled from "styled-components";
-import ModalUnstyled from '@mui/base/ModalUnstyled';
-import {doc, deleteDoc} from "firebase/firestore";
-import {db} from "../../firebase";
+import ModalUnstyled from "@mui/base/ModalUnstyled";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase";
 import { onSnapshot, collection } from "firebase/firestore";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
 
-
-import AddCourseModal from './AddCourse';
+import AddCourseModal from "./AddCourse";
 
 const Styles = styled.div`
   padding: 1rem;
@@ -50,9 +49,8 @@ const Styles = styled.div`
         border-right: 0;
       }
     }
-
   }
-`
+`;
 
 const StyledModal = styled(ModalUnstyled)`
   position: fixed;
@@ -66,7 +64,7 @@ const StyledModal = styled(ModalUnstyled)`
   justify-content: center;
 `;
 
-const Backdrop = styled('div')`
+const Backdrop = styled("div")`
   z-index: -1;
   position: fixed;
   right: 0;
@@ -78,130 +76,95 @@ const Backdrop = styled('div')`
 `;
 
 const style = {
-  bgcolor: 'white',
-  border: '1px solid',
-  borderRadius: '5px',
+  bgcolor: "white",
+  border: "1px solid",
+  borderRadius: "5px",
   p: 2,
   px: 4,
   pb: 3,
 };
 
-// function Table({ columns, data }) {
-//   // Use the state and functions returned from useTable to build your UI
-//   const {
-//     getTableProps,
-//     getTableBodyProps,
-//     headerGroups,
-//     rows,
-//     prepareRow,
-//   } = useTable({
-//     columns,
-//     data,
-//   })
-
-//   // Render the UI for your table
-//   return (
-//     <table {...getTableProps()}>
-//       <thead>
-//         {headerGroups.map(headerGroup => (
-//           <tr {...headerGroup.getHeaderGroupProps()}>
-//             {headerGroup.headers.map(column => (
-//               <th {...column.getHeaderProps()} style={{backgroundColor: "rgb(1, 29, 67)", color: "white", fontWeight: "500"}}>{column.render('Header')}</th>
-//             ))}
-//           </tr>
-//         ))}
-//       </thead>
-//       <tbody {...getTableBodyProps()}>
-//         {rows.map((row, i) => {
-//           prepareRow(row)
-//           return (
-//             <tr {...row.getRowProps()}>
-//               {row.cells.map((id, cell) => {
-//                 console.log(i, cell)
-//                 return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-//               })}
-//             </tr>
-//           )
-//         })}
-//       </tbody>
-//     </table>
-//   )
-// }
-
 const TableView = () => {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Course Code',
-        accessor: 'course_code'
+        Header: "Course Code",
+        accessor: "course_code",
       },
       {
-        Header: 'Course Name',
-        accessor: 'course_name'
+        Header: "Course Name",
+        accessor: "course_name",
       },
       {
-        Header: 'Counted Towards',
-        accessor: 'counted_towards'
+        Header: "Counted Towards",
+        accessor: "counted_towards",
       },
       {
-        Header: 'Credits',
-        accessor: 'credits'
+        Header: "Credits",
+        accessor: "credits",
       },
     ],
     []
-  )
+  );
   // const data = [{course_code: 'ES654', course_name: 'Machine Learning', counted_towards: '-', credits: 4},
   //               {course_code: 'MA504', course_name: 'Linear Algebra', counted_towards: '-', credits: 4}]
-  
+
   const [data, setdata] = useState([]);
   const [uid, setuid] = useState("");
   const [open, setOpen] = React.useState();
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [credits, setcredits] = useState(0);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setuid(user.uid);
-        
       }
     });
   }, []);
 
   useEffect(() => {
-    if (uid){
-    let docRef = collection(db, "students", uid, "coursesOpted");
-    onSnapshot(docRef, (snapshot) =>{
-      let arr = [];
-      snapshot.forEach(doc =>{
-        // setdata(...data, {course_code: doc.id, course_name: doc.data().name, counted_towards: doc.data().countedTowards, credits: doc.data().credits})
-        // data1.add({course_code: doc.id, course_name: doc.data().name, counted_towards: doc.data().countedTowards, credits: doc.data().credits})
-        // console.log(doc.data())
-        let obj = {course_code: doc.id, course_name: doc.data().name, counted_towards: doc.data().countedTowards, credits: doc.data().credits}
-        arr.push(obj);
-      })
-      setdata(arr);
-    })
-  }
-  }, [uid, open])
+    if (uid) {
+      let docRef = collection(db, "students", uid, "coursesOpted");
+      onSnapshot(docRef, (snapshot) => {
+        let arr = [];
+        let creds = 0;
+        snapshot.forEach((doc) => {
+          // setdata(...data, {course_code: doc.id, course_name: doc.data().name, counted_towards: doc.data().countedTowards, credits: doc.data().credits})
+          // data1.add({course_code: doc.id, course_name: doc.data().name, counted_towards: doc.data().countedTowards, credits: doc.data().credits})
+          // console.log(doc.data())
+          let obj = {
+            course_code: doc.id,
+            course_name: doc.data().name,
+            counted_towards: doc.data().countedTowards,
+            credits: doc.data().credits,
+          };
+          creds += doc.data().credits;
+          arr.push(obj);
+        });
+        setdata(arr);
+        setcredits(creds);
+      });
+    }
+  }, [uid, open]);
 
   const [tobedelete, settobedelete] = useState({});
-  useEffect(() =>{
+  useEffect(() => {
     let o1 = {};
-    data.map(c =>{
-      o1[c.course_code]=false;
-    })
+    data.map((c) => {
+      o1[c.course_code] = false;
+    });
     settobedelete(o1);
-  }, [data])
+  }, [data]);
 
-
-  const deleteCourse = async () =>{
-    Object.entries(tobedelete).map(([key, val])=>{
-      if(val){
-        deleteDoc(doc(db, "students", uid, "coursesOpted", key))
+  const deleteCourse = async () => {
+    Object.entries(tobedelete).map(([key, val]) => {
+      if (val) {
+        deleteDoc(doc(db, "students", uid, "coursesOpted", key));
       }
-    })
-  }
+    });
+  };
 
   const handleChange = (event) => {
     settobedelete({
@@ -213,36 +176,62 @@ const TableView = () => {
   return (
     <Styles>
       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead sx={{backgroundColor: 'purple'}}>
-          <TableRow>
-            <TableCell><b>Course Code</b></TableCell>
-            <TableCell align="right"><b>Course Name</b></TableCell>
-            <TableCell align="right"><b>Counted Towards</b></TableCell>
-            <TableCell align="right"><b>Credits</b></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: "purple" }}>
+            <TableRow>
+              <TableCell>
+                <b>Course Code</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Course Name</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Counted Towards</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Credits</b>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.course_code}
+                // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Checkbox name={row.course_code} onChange={handleChange} />
+                  {row.course_code}
+                </TableCell>
+                <TableCell align="right">{row.course_name}</TableCell>
+                <TableCell align="right">{row.counted_towards}</TableCell>
+                <TableCell align="right">{row.credits}</TableCell>
+              </TableRow>
+            ))}
             <TableRow
-              key={row.course_code}
-              // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-              <Checkbox name={row.course_code} onChange={handleChange}/>
-                {row.course_code}
+                Total
               </TableCell>
-              <TableCell align="right">{row.course_name}</TableCell>
-              <TableCell align="right">{row.counted_towards}</TableCell>
-              <TableCell align="right">{row.credits}</TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right">{credits}</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
       <ButtonGroup>
-        <Button variant='outlined' onClick={handleOpen} style={{marginRight: "35px"}}>Add Course</Button>
-        <Button variant='outlined' onClick={deleteCourse} color='error'>Drop Course</Button>
+        <Button
+          variant="outlined"
+          onClick={handleOpen}
+          style={{ marginRight: "35px" }}
+        >
+          Add Course
+        </Button>
+        <Button variant="outlined" onClick={deleteCourse} color="error">
+          Drop Course
+        </Button>
       </ButtonGroup>
       <Modal>
         <StyledModal
@@ -253,13 +242,17 @@ const TableView = () => {
           BackdropComponent={Backdrop}
         >
           <Box sx={style}>
-            <AddCourseModal uid={uid} handleClose={handleClose} setdata={setdata}/>
+            <AddCourseModal
+              uid={uid}
+              handleClose={handleClose}
+              setdata={setdata}
+            />
           </Box>
         </StyledModal>
       </Modal>
     </Styles>
-  )
-}
+  );
+};
 
 const ButtonGroup = styled.div`
   margin-top: 20px;
@@ -267,7 +260,6 @@ const ButtonGroup = styled.div`
   display: flex;
 `;
 
-const Modal = styled.div`
-`;
+const Modal = styled.div``;
 
 export default TableView;
